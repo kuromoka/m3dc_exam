dbb5d68a5043e37189e5af08f7feb343cedc9b2e
kuromoka <kuromokapb@gmail.com>
2018-12-01 16:30:18 +0900

    DB登録処理を実装

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Formatter\LineFormatter;
+use App\Seminar;
 
 class InputController extends Controller
 {
@@ -17,11 +18,13 @@ class InputController extends Controller
     
     public function displayview(Request $request)
     {
+        // ログ出力は以下を参考にした
+        // http://sagatto.com/20180228_laravel_custom_log
+        // http://kzhishu.hatenablog.jp/entry/2015/10/04/200000
         $logger = new Logger('exam_log');
         $log_path = base_path() . '/logs/' . Carbon::now()->format("Y_m_d H:i:s") . '.log';
         $log_level =  config('app.log_level');
         $log_format = "[%datetime%] %message%\n";
-
         $handler = new StreamHandler($log_path, $log_level);
         $handler->setFormatter(new LineFormatter($log_format));
         $logger->pushHandler($handler);
@@ -35,7 +38,16 @@ class InputController extends Controller
             'referer' => url()->previous(),
             'usr_agent' => $request->userAgent(),
         );
+        // ログファイルに出力
         $logger->addDebug(implode(",", array_values($exam_log_data)));
+
+        // seminarsテーブルに登録
+        Seminar::create(
+            array_merge(
+                array('crnt_date' => Carbon::now()->toDateTimeString()),
+                $exam_log_data
+            )
+        );
 
         return view('viewpages.viewpage');
     }
