23f0614691d81f1b4f537cccfb55c8a1c3eb434c
kuromoka <kuromokapb@gmail.com>
2018-12-01 13:23:00 +0900

    入力ページに項目追加

--- resources/views/viewpages/input.blade.php
+++ resources/views/viewpages/input.blade.php
@@ -16,11 +16,11 @@
                         			<div class="gaiyo">
                                     	<dl>
                                     		<dt>日程</dt>
-	                                			<dd></dd>
+	                                			<dd>{{ Config::get('defaultcfg.defaultcfg.SEMI_INFO_DATE') }}</dd>
 	                                			<dt>演題</dt>
-	                                			<dd></dd>
+	                                			<dd>{{ Config::get('defaultcfg.defaultcfg.SEMI_INFO_TITLE') }}</dd>
 	                                			<dt>演者</dt>
-	                                			<dd></dd>
+	                                			<dd>{{ Config::get('defaultcfg.defaultcfg.SEMI_INFO_PROF') }}</dd>
                                     	</dl>
                                     </div>
                         		</div>
@@ -42,10 +42,25 @@
                             					<div class="col-md-4">
                                 					<select name="todohuken" id="todohuken" class="form-control">
                                 					<option value="msg" selected="selected" class="msg">都道府県</option>
+													<option value="1" class="msg">東京都</option>
                             						</select>
-                        							</div>
                     							</div>
-
+										</div>
+										<div class="form-group">
+											<label class="col-md-2 control-label" >ご氏名</label>
+                            					<div class="col-md-4">
+                                					<input type="text" name="fname" id="fname" class="form-control">
+                    							</div>
+                            					<div class="col-md-4">
+                                					<input type="text" name="lname" id="lname" class="form-control">
+                    							</div>
+										</div>
+										<div class="form-group">
+											<label class="col-md-2 control-label" >参加人数</label>
+                            					<div class="col-md-4">
+                                					<input type="number" name="viewcnt" id="viewcnt" class="form-control">
+                    							</div>
+										</div>
 
     														<div class="row">
 		                              <div class="btn-group btn-group-justified">
