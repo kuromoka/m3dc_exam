c5915681390649f10fb3c2b0a4420acb2d6b6c27
kuromoka <kuromokapb@gmail.com>
2018-12-01 15:33:43 +0900

    ログ出力の処理を実装

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
 
 class InputController extends Controller
 {
@@ -11,8 +15,28 @@ class InputController extends Controller
         return view('viewpages.input');
     }
     
-    public function displayview()
+    public function displayview(Request $request)
     {
+        $logger = new Logger('exam_log');
+        $log_path = base_path() . '/logs/' . Carbon::now()->format("Y_m_d H:i:s") . '.log';
+        $log_level =  config('app.log_level');
+        $log_format = "[%datetime%] %message%\n";
+
+        $handler = new StreamHandler($log_path, $log_level);
+        $handler->setFormatter(new LineFormatter($log_format));
+        $logger->pushHandler($handler);
+
+        $exam_log_data = array(
+            'todohuken' => $request->todohuken,
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+            'viewcnt' => $request->viewcnt,
+            'ip_addr' => $request->ip(),
+            'referer' => url()->previous(),
+            'usr_agent' => $request->userAgent(),
+        );
+        $logger->addDebug(implode(",", array_values($exam_log_data)));
+
         return view('viewpages.viewpage');
     }
 }
