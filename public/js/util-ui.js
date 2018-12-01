0d8b823cdfe49fb0e596b8c4c792a7d50f3a98e9
kuromoka <kuromokapb@gmail.com>
2018-12-01 13:23:32 +0900

    バリデーションルールを追加

--- public/js/util-ui.js
+++ public/js/util-ui.js
@@ -10,13 +10,35 @@ $(function () {
             todohuken: {
                 valueNotEquals: "msg"
             },
-
+            fname: {
+                required: true,
+                maxlength: 50,
+            },
+            lname: {
+                required: true,
+                maxlength: 50,
+            },
+            viewcnt: {
+                required: true,
+                number: true,
+            },
         },
         messages: {
             todohuken: {
                 equalTo: "選択してください"
             },
-
+            fname: {
+                required: "必須項目です",
+                maxlength: "50文字以下で入力してください",
+            },
+            lname: {
+                required: "必須項目です",
+                maxlength: "50文字以下で入力してください",
+            },
+            viewcnt: {
+                required: "必須項目です",
+                number: "数字のみ入力してください",
+            },
         },
         errorClass: "error_msg",
         wrapper: "li"
